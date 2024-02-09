6be4f29c2cd46c2bf87c5def7312f21296862890
pollydrews <136692214+pollydrews@users.noreply.github.com>
2024-02-09 14:55:25 +0000

    Add new screens

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,15 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+    
+
+// Record consent qual 1
+    router.post('/consent-valid', function(request, response) {
+
+        var consentValid = request.session.data['is-consent-valid']
+        if (consentValid == "yes"){
+            response.redirect("manage/verification/verification-overview-valid-consent")
+        } else if (consentValid == "no") {
+            response.redirect("manage/verification/qual1-send-consent-for-review")
+        }
+    })
